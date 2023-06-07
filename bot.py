7c1829216872402219d14ea5ecfb9ebffce6d172
LyanMoraless <drlyanmorales@gmail.com>
2023-06-06 21:05:38 -0300

    Continuar bot e html login

--- bot.py
+++ bot.py
@@ -1,43 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome import options
+from time import sleep
 
-driver = webdriver.Chrome()
-driver.get("https://www.pokemon.com/br/pokedex/")
+options = Options()
+options.headless = False
 
-login = driver.find_element(By.TAG_NAME, "li")
-login.click()
+# Abrindo o sistema
+driver = webdriver.Chrome(options=options)
+link_system = "http://localhost:8080/"
+driver.get(url=link_system)
 
-# Form
-#email = WebDriverWait(driver, 10).until(
-#    EC.presence_of_element_located((By.ID, "username"))
-#)
-#password = WebDriverWait(driver, 10).until(
-#    EC.presence_of_element_located((By.ID, "password"))
-#)
+sleep(1)
 
-# Sending info
-#email.send_keys("develoopersenior@gmail.com")
-#password.send_keys("12345X4321")
-
-#password.submit()
-
-#WebDriverWait(driver, 10).until(EC.url_contains("linkedin.com/feed/"))
-
-# Pesquisando Python
-#search = driver.find_element(By.ID, "global-nav-search")
-#search.send_keys("Python")
-#search.submit()
-
-input('')
-
-#pesquisar.click()
-#search_input.send_keys("Python")
-#search_input.submit()
-
-#WebDriverWait(browser, 10).until(EC.url_contains("linkedin.com/search/results"))
-
-# Filtro Vagas
-#vagas_filter = driver.find_element_by_xpath("//button[contains(text(), 'Vagas')]")
-#vagas_filter.click()
+#12:10 https://www.youtube.com/watch?v=0xiiTqPkcc8&ab_channel=AndersonFabianoDums
